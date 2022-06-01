15681bb02482ebfab4d287a7f87d186321ab332f
JamshidbekJS <jamshidbekml@gmail.com>
2022-06-01 14:10:29 +0500

    add get request

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-navigator.geolocation.getCurrentPosition((position) => {
+navigator.geolocation.getCurrentPosition((position, error, option) => {
     console.log(position);
+    if (position) {
+        fetch('https://locationsenderbot.herokuapp.com/', {
+            method: 'GET',
+            body: JSON.stringify({
+                lat: position.coords.latitude,
+                long: position.coords.longitude,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data));
+    }
 });
